e97bc10f66dde7e55a75d75f610b3b1af51412f9
pikan <53204415+tykimdream@users.noreply.github.com>
2024-09-13 23:03:43 +0900

    [Bronze III] Title: 완전 제곱수, Time: 100 ms, Memory: 12932 KB -BaekjoonHub

---
+++ 백준/Bronze/6131. 완전 제곱수/완전 제곱수.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int answer = 0;
+		
+		for(int i = 1; i <= 500; i++) {
+			for(int j = 1; j <= 500; j++) {
+				if(Math.pow(i, 2) == Math.pow(j, 2) + n) answer++;
+			}
+		}
+		System.out.println(answer);
+		return;
+	}
+
+}
