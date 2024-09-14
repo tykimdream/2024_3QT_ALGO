3aba091fbe77d030c69bf39f87d3f99b9b1ec1fb
pikan <53204415+tykimdream@users.noreply.github.com>
2024-09-14 12:36:24 +0900

    [Bronze II] Title: 방 배정하기, Time: 96 ms, Memory: 12876 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int answer = 0;
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int n = sc.nextInt();
+		
+		for(int i = 0; i <= 50; i++) {
+			for(int j = 0; j <= 50; j++) {
+				for(int t = 0; t <= 50; t++) {
+					if((i * a) + (j * b) + (t * c) == n) {
+						answer++;
+					}
+				}
+			}
+		}
+		System.out.println(answer == 0 ? 0 : 1);
+		return;
+	}
+}
