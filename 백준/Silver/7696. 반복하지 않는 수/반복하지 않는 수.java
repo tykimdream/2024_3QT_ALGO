b641fd8b201da37feb65c6bb400c2f889b6f42cd
pikan <53204415+tykimdream@users.noreply.github.com>
2024-09-16 12:11:51 +0900

    [Silver III] Title: 반복하지 않는 수, Time: 652 ms, Memory: 17028 KB -BaekjoonHub

---
+++ 백준/Silver/7696. 반복하지 않는 수/반복하지 않는 수.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static final int MAX = 1000000;
+    static int[] nonDuplicateNumbers = new int[MAX + 1];
+    static boolean[] used = new boolean[10];
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        precompute();
+        
+        while (true) {
+            int t = sc.nextInt();
+            if (t == 0) break;
+            System.out.println(nonDuplicateNumbers[t]);
+        }
+    }
+
+    public static void precompute() {
+        int count = 1;
+        for (int i = 1; count <= MAX; i++) {
+            if (isNonDuplicateNumber(i)) {
+                nonDuplicateNumbers[count++] = i;
+            }
+        }
+    }
+
+    public static boolean isNonDuplicateNumber(int n) {
+    	Arrays.fill(used, false);
+    	
+        while (n > 0) {
+            int digit = n % 10;
+            if (used[digit]) return false;
+            used[digit] = true;
+            n /= 10;
+        }
+        return true;
+    }
+}
