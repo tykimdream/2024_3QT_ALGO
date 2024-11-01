57dac4191b0f4307d73388d66f84c2c74cfdb826
pikan <53204415+tykimdream@users.noreply.github.com>
2024-11-01 19:12:23 +0900

    [Gold V] Title: 회문, Time: 424 ms, Memory: 35668 KB -BaekjoonHub

---
+++ 백준/Gold/17609. 회문/회문.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			char[] arr = sc.next().toCharArray();
+			System.out.println(isPan(arr, 0, arr.length - 1, 0));
+		}
+	}
+
+	public static int isPan(char[] arr, int start, int end, int deleted) {
+		while (start < end) {
+			if (arr[start] != arr[end]) {
+				if (deleted == 0) {
+					// 문자를 아직 삭제하지 않은 경우만 추가 검사
+					if (isPan(arr, start + 1, end, 1) == 0) return 1;
+					if(isPan(arr, start, end - 1, 1) == 0) return 1;
+					return 2;
+				} else {
+					// 이미 문자를 한 번 삭제했는데 또 다른 문자면 불가능
+					return 2;
+				}
+			}
+			start++;
+			end--;
+		}
+		return 0;
+	}
+}
