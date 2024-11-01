d349ca130526c1132fe2624f2433322439e58200
pikan <53204415+tykimdream@users.noreply.github.com>
2024-11-01 17:00:18 +0900

    [Silver II] Title: 최대 페이지 수, Time: 72 ms, Memory: 11608 KB -BaekjoonHub

---
+++ 백준/Silver/16493. 최대 페이지 수/최대 페이지 수.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int[][] chaps = new int[m + 1][2];
+		
+		for(int i = 1; i <= m; i++) {
+			 st = new StringTokenizer(br.readLine());
+
+			chaps[i][0] = Integer.parseInt(st.nextToken());
+			chaps[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		int[][] dp = new int[m + 1][n + 1];
+				
+		for(int i = 1; i <= m; i++) {
+			int day = chaps[i][0];
+			int page = chaps[i][1];
+			for(int d = 1; d <= n; d++) {
+				dp[i][d] = dp[i - 1][d];
+				if(d >= day) {
+					dp[i][d] = Math.max(dp[i][d], dp[i - 1][d - day] + page);
+				}
+			}
+		}
+		System.out.println(dp[m][n]);
+	}
+
+}
