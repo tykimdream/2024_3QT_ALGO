497be967c2bf9355c9cf5c2136e96aeb76c70102
pikan <53204415+tykimdream@users.noreply.github.com>
2024-11-05 01:03:11 +0900

    [level 1] Title: 체육복, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.3 MB, 시간: 0.13 ms
+메모리: 33.4 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 12일 01:31:55
+2024년 11월 05일 01:03:07
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,20 +1,26 @@
 function solution(n, lost, reserve) {
-    let students = Array(n).fill(1);
+    var answer = 0;
+    const student = Array(n).fill(1);
+    for(r of reserve){
+        student[r - 1]++;
+    }
     
-    for(let l of lost) students[l - 1]--;
-    for(let r of reserve) students[r - 1]++;
+    for(l of lost){
+        student[l - 1]--;
+    }
     
     for(let i = 0; i < n; i++){
-        if(students[i] == 0){
-            if(students[i - 1] == 2){
-                students[i - 1] = 1;
-                students[i] = 1;
-            } else if(students[i + 1] == 2){
-                students[i + 1] = 1;
-                students[i] = 1;
+        if(student[i] == 0){
+            if(student[i - 1] && student[i - 1] > 1){
+                student[i]++;
+                student[i - 1]--;
+            } else if(student[i + 1] && student[i + 1] > 1){
+                student[i]++;
+                student[i + 1]--;
             }
         }
     }
     
-    return students.filter(e => e > 0).length;
+    
+    return student.filter(e => e > 0).length;
 }
