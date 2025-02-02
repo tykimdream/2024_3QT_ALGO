6b1564abfd6eef699f6ec12b46de682da406c073
pikan <53204415+tykimdream@users.noreply.github.com>
2025-02-02 20:41:31 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 35.48 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -0,0 +1,29 @@
+function solution(gems) {
+    const gemTypes = new Set(gems).size;
+    const counter = {};
+    const len = gems.length;
+    let start = 0, end = 0;
+    let currentTypes = 0;
+    let answer = [1, len];
+    
+    for(;end < len; end++){
+        if(!counter[gems[end]]){
+            currentTypes++;
+        }
+        counter[gems[end]] = (counter[gems[end]] || 0) + 1;
+        
+        while(currentTypes === gemTypes){
+            if(end - start < answer[1] - answer[0]){
+                answer = [start + 1, end + 1];
+            }
+            
+            counter[gems[start]]--;
+            if(counter[gems[start]] === 0){
+                currentTypes--;
+            }
+            start++;
+        }
+    }
+    
+    return answer;
+}
